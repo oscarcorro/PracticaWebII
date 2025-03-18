6d36f1162406df36af34a40a833418e45178f084
oscarcorro <oscarcorrochanolopez@gmail.com>
2025-03-18 12:46:36 +0100

    Modelo de User completado

---
+++ models/nosql/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const mongooseDelete = require("mongoose-delete")
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: { //nombre del usuario string
+            type: String
+        },
+        lastname: { //apellido del usuario string
+            type: String
+        },
+        nif: { //nif del usuario string
+            type: String
+        },
+        company: { //compañia/datos si es autonomo con nombre cif y direccion
+            name: String,
+            cif: String,
+            address: String
+        },
+        email: { //email string unico, obligatorio sin espacios
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        password: { //contraseña string obligatoria minimo 8 caracteres
+            type: String,
+            required: true,
+            minlength: 8
+        },
+        status: { //estado del usuario string, verificado o pendiente (por defecto)
+            type: String,
+            enum: ["pending", "verified"],
+            default: "pending"
+        },
+        role: { //rol del usuario string admin o user (por defecto)
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
+        verificationCode: { //codigo de 6 digitos para verificar el email
+            type: String
+        },
+        attemptsLeft: { //intentos para el codigo de verificacion
+            type: Number,
+            default: 3
+        },
+        logo: { //logo de la empresa
+            type: String
+        }
+    },
+    {
+        timestamps: true, //hace createdAt, updatedAt
+        versionKey: false
+    }
+)
+//Utilizamos soft-delete de mongoose para no eliminar los usuarios del todo
+UserSchema.plugin(mongooseDelete, {overrideMethods: "all", deletedAt: true })
+
+module.exports = mongoose.model("User", UserSchema) //exportamos el modelo como User
